e0d760054e0c926655755701d25591086ffc95e8
araj67995 <134071631+araj67995@users.noreply.github.com>
2025-01-05 13:31:05 +0530

    Add files via upload

---
+++ bubble game/script.js
@@ -0,0 +1,47 @@
+let digit  = "";
+let timer = 60;
+let score = 0;
+var hitdigit = 0;
+document.querySelector("#timer").textContent = timer
+
+function runTimer(){
+    var timerint = setInterval(function(){
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timer").textContent = timer
+        }else{
+            clearInterval(timerint);
+            document.querySelector(".game").innerHTML = ` <p class="over">GAME OVER</p> `;
+        }
+    },1000);
+};
+for(let i=1; i<=176; i++){
+    let rn = Math.floor(Math.random()*10);
+    digit +=`<div class="digit">${rn}</div>`;
+}
+function hit(){
+    hitdigit = Math.floor(Math.random()*10);
+    document.querySelector("#hit").textContent = hitdigit;
+}
+function increaseScore (){
+    score += 10;
+    document.querySelector("#score").textContent = score;
+}
+
+runTimer();
+document.querySelector(".game").innerHTML = digit;
+hit();
+
+
+document.querySelector(".game")
+.addEventListener("click", function(dets){
+    let clickedNum = Number(dets.target.textContent);
+     if(clickedNum === hitdigit){
+       increaseScore();
+       hit();
+     }
+});
+
+
+
+    
